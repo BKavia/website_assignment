6de2e8f6242fdfe067d4d98c20cd81d3c1fe7e97
BKavia <p14178151@my365.dmu.ac.uk>
2017-12-11 22:01:28 +0000

    Added images, javascript validation and page header changes

---
+++ js/contact_page_scripts.js
@@ -0,0 +1,53 @@
+(function () {
+
+	var name = document.getElementById("txtName");
+	var email = document.getElementById("txtEmail");
+	var subject = document.getElementById("txtSubject");
+	var message = document.getElementById("txtMessage");
+	var valMessage = document.getElementById("message");
+	var submitBtn = document.getElementById("submitBtn");
+	
+	submitBtn.addEventListener("click", validate);
+
+	
+	function validate() {
+		var hasError = false; 
+		hasError = validateField(name, valMessage);
+		hasError = validateField(email, valMessage);
+		hasError = validateField(subject, valMessage);
+		hasError = validateField(message, valMessage);
+		
+				
+		if (hasError){
+			valMessage.innerHTML = "Please provide all details";
+		}else{
+			valMessage.innerHTML = "Thanks for your submission.";
+			name.value = '';
+			email.value = '';
+			subject.value = '';
+			message.value = '';
+		}
+	}
+	
+	function validateField(field, valMessage){
+		var isFailed = false; 
+		if (field.value.trim().length == 0) {
+			isFailed = true;
+			displayError(field, valMessage, true);
+		}else{
+			displayError(field, valMessage, false);
+		}
+		return isFailed;
+	}
+	
+	function displayError(field, valMessage, isError){
+		if (isError){
+			field.classList.add("error");
+			valMessage.classList.add("error_msg");
+		}else{
+			field.classList.remove("error");
+			valMessage.classList.remove("error_msg");
+		}
+	}
+
+})();
