3062b47c3f5e734290de4d359747b5913e5dd060
BKavia <p14178151@my365.dmu.ac.uk>
2017-12-21 22:11:44 +0000

    Add product carousel on homepage

---
+++ js/carousel.js
@@ -0,0 +1,31 @@
+
+
+
+(function () {
+
+	let leftButton = document.getElementById("leftButton");
+	let rightButton = document.getElementById("rightButton");
+
+	leftButton.addEventListener("click", moveLeft);
+	rightButton.addEventListener("click", moveRight);
+	
+	let currentPos = 0;
+	let containerWidth = document.getElementById("carousel_container").offsetWidth;
+	let carouselDiv = document.getElementById("carousel");
+
+	function moveRight(){
+	  if (containerWidth >= currentPos ){
+		  currentPos = (currentPos + 100);
+		  carouselDiv.style.transform = "translateX("+currentPos+"px)";
+	 }
+	}
+
+	function moveLeft(){
+	  currentPos = (currentPos - 100);
+	  if (currentPos < -containerWidth ){
+			currentPos = -containerWidth;
+	  }
+	  carouselDiv.style.transform = "translateX("+currentPos+"px)";
+	 }
+	
+ })();
