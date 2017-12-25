1bc5acb9e0f0778b3bc0f370ce92661a1f67c8bb
BKavia <p14178151@my365.dmu.ac.uk>
2017-12-25 23:12:39 +0000

    Limiting the scroll on the product carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,4 +1,3 @@
-
 (function () {
 
 	let leftButton = document.getElementById("leftButton");
@@ -6,32 +5,27 @@
 
 	leftButton.addEventListener("click", moveLeft);
 	rightButton.addEventListener("click", moveRight);
-	
+
 	let currentPos = 0;
-	let containerWidth = document.getElementById("carousel_container").offsetWidth;
+	let currentIndex = 1;
+	let articleWidth = 190;
+	let noOfArticles = document.getElementsByClassName("collection-article").length;
 	let carouselDiv = document.getElementById("carousel");
-	let carouselDivLeft = document.getElementById("carousel").offsetLeft;
 	
-	function moveLeft(){
-		  console.log("left",currentPos, carouselDivLeft);
-		  
-		  //if (currentPos <= 0){
-		//	  currentPos = 0;
-		 // }else{
-			  currentPos = (currentPos + 190);
-		 // }
-		  
-		  carouselDiv.style.transform = "translateX("+currentPos+"px)";
+	function moveLeft() {
+		if (currentIndex > 1) {
+			currentPos = (currentPos + articleWidth);
+			currentIndex--;
+		}
+		carouselDiv.style.transform = "translateX(" + currentPos + "px)";
 	}
 
-	function moveRight(){
-		    console.log(currentPos, carouselDivLeft);
-	
-	  currentPos = (currentPos - 190);
-	  if (currentPos < -containerWidth ){
-			currentPos = -containerWidth;
-	  }
-	  carouselDiv.style.transform = "translateX("+currentPos+"px)";
-	 }
-	
- })();
+	function moveRight() {
+		if (currentIndex < noOfArticles) {
+			currentPos = (currentPos - articleWidth);
+			currentIndex++;
+		}
+		carouselDiv.style.transform = "translateX(" + currentPos + "px)";
+	}
+
+})();
